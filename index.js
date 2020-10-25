1b06f6cb8eeb04b5af2103be85e44372a9afd784
Matt Love <matthew.love683@gmail.com>
2020-10-25 00:23:31 -0400

    converter

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
-function validate(); {
-  let signup = document.getElementById("signup")
-  let name = document.getElementById("name")
-  let email = document.getElementById("email")
-  let number = document.getElementById("number")
-  let error = document.getElementById("error")
-
+function validate() {
+  //fields
+  let name = document.getElementById("name");
+  let email = document.getElementById("email");
+  let number = document.getElementById("number");
+  //form
+  let form = document.getElementById("form");
+  //error message
+  let error = document.getElementById("error");
+  //variable message
   let message;
-  if(name.length < 6 || name.value == "") {
-    message = ("Full name is required!")
+  //if name is blank or less/equal to 6 characters
+  if(name.value == "" || name.length <= 6) {
+    message = "Name must be filled out"
     error.innerHTML = message;
     return false;
-  } 
-
-  if(email.length < 10 || email.indexOf("@") == -1){
-    message = ("Valid email is required!")
-    error.innerText = message;
-  }
-
-  if(number.length < 10){
-    message = ("Area code must be entered!")
-    error.innerText = message;
   }
   
+  //if email is blank or doesn't contain @
+  if(email.value == "" || email.indexOf("@") == -1) {
+    message = "Email must be filled out"
+    error.innerHTML = message;
+    return false;
+  }
 
+  //if phone number doesn't include area code
+  if(number.value == "" || number.length < 10) {
+    message = "Must enter full phone number"
+    error.innerHTML = message;
+    return false;
+  }
+  alert ("Thank you for your support");
+  return true;
+ 
+  
 }
-  
-      
-      
-    
-
-   
-  
-    
-    
-    
-    
-    
-     
-
-
-    
-
