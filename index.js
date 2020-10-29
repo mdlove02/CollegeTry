d29f1d48cc66bda37a8c7d76ee4a626cb4fdd7c5
Matt Love <matthew.love683@gmail.com>
2020-10-28 22:33:03 -0400

    form js

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ function validate() {
   //variable message
   let message;
   //if name is blank or less/equal to 6 characters 
-  if(name.value === null) {
+  if(name.length == 0) {
     message = "All fields must be filled out"
     error.innerHTML = message;
     return false;
-  } else if(name.length < 6) {
+  } else if(name.length >=1 && name.length <= 6) {
     message = "Full name must be used"
     error.innerHTML = message;
     return false;
-    }
+    } 
   
   //if email is blank or doesn't contain @
-  if(email.value === null) {
+  if(email.length == 0) {
     message = "Email field cannot be empty"
     error.innerHTML = message;
     return false;
@@ -32,17 +32,17 @@ function validate() {
     }
 
   //if phone number doesn't include area code
-  if(number.value === null) {
+  if(number.length == 0) {
     message = "Field must be filled out"
     error.innerHTML = message;
     return false;
-  } else if(number.length < 10) {
+  } else if(number.length >=1 && number.length < 10) {
     message = "Must enter full phone number"
     error.innerHTML = message;
     return false;
     }
 
-  alert ("Thank you for your support");
+  alert ("Thank you for your support" + 'name');
   return true;
 
   
