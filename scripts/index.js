714c93813589bbe852d0b404a311a1cd4e23d436
Matt Love <matthew.love683@gmail.com>
2020-11-16 20:38:09 -0500

    done

--- scripts/index.js
+++ scripts/index.js
@@ -7,13 +7,14 @@ function validate() {
   let number = document.getElementById("number").value;
  
   //error message
-  let error = document.getElementById("error"); //create an error message array so it throws out all errors possible 
+  let error = document.getElementById("error"); 
   
+  //create an error message array so it throws out all errors possible (future)
   //let error = ["All fields must be filled out", "Full name must be used", "Valid email musty be used", "Phone number must include area code"];
 
   //variable message
   let message;
-  //if name is blank or less/equal to 6 characters 
+  //if name is blank (0) or less/equal to 6 characters 
   if(name.length == 0) {
     message = "All fields must be filled out"
     error.innerHTML = message;
@@ -24,7 +25,7 @@ function validate() {
     return false;
     } 
   
-  //if email is blank or doesn't contain @
+  //if email is blank (0) or doesn't contain @
   if(email.length == 0) {
     message = "Email and Phone field must be filled out"
     error.innerHTML = message;
@@ -35,7 +36,7 @@ function validate() {
     return false;
     }
 
-  //if phone number doesn't include area code
+  //if phone number is blank (0) or doesn't contain area code. - will count as character so number can be filled out both ways.
   if(number.length == 0) {
     message = "Phone number field must be filled out"
     error.innerHTML = message;
@@ -46,13 +47,13 @@ function validate() {
     return false;
     }
 
-  alert ("Thank you for your support" + " " + name);
+  alert ("Thank you for your support" + " " + name); //adds name that was filled in to message
   return true;
 
 }
 
 /*Calculation*/
-
+// fields
 function changeWeight() {
   let input = document.getElementById("input");
   let result = document.getElementById("result");
